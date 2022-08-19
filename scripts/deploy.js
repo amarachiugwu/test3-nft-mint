92e5fc4e5995f36f56a1db7f02c3ad7179f48bfd
amarachiugwu <marycynthiamara2@gmail.com>
2022-08-19 19:25:37 +0100

    initial upload

---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+async function main() {
+
+    // Get our account (as deployer) to verify that a minimum wallet balance is available
+    const [deployer] = await ethers.getSigners();
+
+    console.log(`Deploying contracts with the account: ${deployer.address}`);
+    console.log(`Account balance: ${(await deployer.getBalance()).toString()}`);
+
+    // Fetch the compiled contract using ethers.js
+    const NFT = await ethers.getContractFactory("NFT");
+    // calling deploy() will return an async Promise that we can await on 
+    const nft = await NFT.deploy();
+
+    console.log(`Contract deployed to address: ${nft.address}`);
+}
+
+main()
+.then(() => process.exit(0))
+.catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
+
+// contract address on rinkyby
+// 0xc7a0F47BFEBe8e1780de4C06A0a66f147ebFF298
